4f1d74686170d0761bc4a8f4e3c2ff4cea5d22df
mikosetiawan <mikosetiawan152@gmail.com>
2023-12-28 15:22:29 +0700

    menambah barcode

--- loketHome.php
+++ loketHome.php
@@ -6,10 +6,7 @@
   <link rel="stylesheet" href="loketHome.css" />
 
   <!-- boostraps 5.3 -->
-  <!-- <link
-      rel="css/stylesheet"
-      href="node_modules/bootstrap/dist/css/bootstrap.min.css"
-    /> -->
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
 </head>
 
@@ -28,12 +25,8 @@
           <div class="card text-center">
             <div class="card-header">Nomor Antrian Anda</div>
             <div class="card-body">
-              <form action="counter.php" method="post" class="d-flex flex-column">
-                <input type="text" class="loket_nomor" value="" />
-                <button type="submit" name="loket_add" class="counter btn btn-primary fs-2">
-                  Ambil Antrian
-                </button>
-              </form>
+                <div class="nomor"></div>
+                <button type="submit" onclick="ambilNomorAntrian()" class="counter btn btn-primary fs-2">Ambil Antrian</button>
             </div>
           </div>
         </div>
@@ -42,13 +35,34 @@
   </main>
   <!-- <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script> -->
   <script>
-    let count = 0;
-    const counter = document.querySelector(".counter");
-    const loketNomor = document.querySelector(".loket_nomor");
-    counter.addEventListener("click", () => {
-      count += 1;
-      loketNomor.innerHTML = count;
-    });
+    // function ambilNomorAntrian() {
+    //   Ambil nomor antrian dari server
+    //   fetch("ambil_nomor_antrian.php")
+    //     .then((response) => response.text())
+    //     .then((nomor_antrian) => {
+    //       // Tampilkan nomor antrian di halaman
+    //       document.querySelector(".loket_nomor").innerHTML = nomor_antrian;
+    //     });
+      
+    // }
+    function ambilNomorAntrian() {
+            // Menggunakan AJAX untuk mengirim permintaan ke server
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    // Menanggapi hasil yang dikembalikan dari server
+                    document.getElementById("nomor").innerHTML = "Nomor Antrian Anda: " + xhr.responseText;
+                    printNomorAntrian(xhr.responseText);
+                }
+            };
+            xhr.open("GET", "ambil_nomor_antrian.php", true);
+            xhr.send();
+        }
+
+        function printNomorAntrian(nomor) {
+            // Menggunakan JavaScript untuk mencetak nomor antrian
+            window.print();
+        }
   </script>
 </body>
 
